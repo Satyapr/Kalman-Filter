ac56bef1b2aa3deab2cd30703c321a47994a6012
satyapr93 <satyapr93@gmail.com>
2016-04-25 20:44:28 +0530

    changed Kalman filter to interface

--- src/main/java/org/agvkgp/utils/filters/KalmanFilter.java
+++ src/main/java/org/agvkgp/utils/filters/KalmanFilter.java
@@ -1,14 +1,8 @@
-public abstract class KalmanFilter {
-	// protected KalmanFilter(final ProcessModel processModelP, final ObservationModel observationModelP);
-	
-	public abstract DoubleVector getStateEstimate();
-	public abstract ProcessModel getProcessModel();
-	public abstract ObservationModel getObservationModel();
-	
-	public abstract void predict(final DoubleVector u);
-	public abstract void update(final DoubleVector z);
+public interface KalmanFilter {
+    DoubleVector stateEstimate();
+    ProcessModel processModel();
+    ObservationModel observationModel();
 
-	public static KalmanFilter create(final ProcessModel processModelP, final ObservationModel observationModelP) {
-		return new StandardKalmanFilter(processModelP, observationModelP);
-	}
+    void predict(final DoubleVector u);
+    void update(final DoubleVector z);
 }
